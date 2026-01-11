d73ce5a333aad38b19976087ea201fbd431d4a62
sujal302004 <sujalkabra30@gmail.com>
2026-01-11 16:50:10 +0530

    Update Translator.jsx

--- src/pages/Translator.jsx
+++ src/pages/Translator.jsx
@@ -17,7 +17,7 @@ export default function Translator() {
           method: "POST",
           headers: {
             "content-type": "application/x-www-form-urlencoded",
-            "X-RapidAPI-Key": "5c4df7f5admshf8d02e2fafd771cp1a33fejsn664d00851ea7",
+           "X-RapidAPI-Key": "YOUR_API_KEY",
             "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
           },
           body: new URLSearchParams({
